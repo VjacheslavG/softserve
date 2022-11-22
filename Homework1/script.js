68d020631e178509e4d928e6094a9dd3d0e1544f
VjacheslavG <102754084+VjacheslavG@users.noreply.github.com>
2022-11-22 14:15:26 +0200

    Add files via upload

---
+++ Homework1/script.js
@@ -0,0 +1,48 @@
+//Task2
+// console.log("Gerasymchuk");
+
+//Task3
+// let a=10, b=20;
+// alert(`${a} ${b}`);
+// a=b;
+// alert(`${a} ${b}`);
+
+//Task4
+// const myObject = {
+//     myString: "myString",
+//     myNumber: 10,
+//     myBoolean: false,
+//     myUndefined: undefined,
+//     myNull:null
+// };
+
+//Task5
+// let isAdult=confirm('are you of legal age?');
+// console.log(isAdult);
+
+//Task6
+// let firstName='First Name';
+// let lastName='Last Name';
+// let group='my group';
+// let myYear=2022;
+// let isMarried=true;
+// console.log(typeof(myYear));
+// console.log(typeof(isMarried));
+// console.log(typeof(firstName));
+// let myNull=null;
+// let myUndefined=undefined;
+// console.log(typeof(myNull));
+// console.log(typeof(myUndefined));
+
+//Task7
+// let userName=prompt('Enter your name');
+// let userEmail=prompt('Enret your email');
+// let userPass=prompt('Enter your password');
+// alert(`Dear ${userName}, your email is ${userEmail}, your password is ${userPass}`);
+
+//Task8
+// let inMinute=60;
+// let inHour=inMinute*60;
+// let inDay=inHour*24;
+// let inMonth=inDay*31;
+// alert(`${inHour} seconds per hour, ${inDay} seconds per day, ${inMonth} seconds per month`);
